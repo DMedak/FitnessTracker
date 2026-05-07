edf846951ca243ae7ceb9fc147474b6915bd1f25
DMedak <dmedak15@gmail.com>
2026-05-07 11:41:14 +0200

    Ažurirane postavke servera i postavljanja aplikacije

--- mobile/src/config/api.ts
+++ mobile/src/config/api.ts
@@ -1,1 +1,11 @@
-export const API_URL = 'http://192.168.1.97:3000';
+import Constants from 'expo-constants';
+
+const debuggerHost =
+  Constants.expoConfig?.hostUri ||
+  Constants.manifest2?.extra?.expoClient?.hostUri;
+
+const localIp = debuggerHost?.split(':')[0];
+
+export const API_URL = localIp
+  ? `http://${localIp}:3000`
+  : 'http://localhost:3000';

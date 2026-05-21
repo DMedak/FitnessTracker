809c6c0264797823b98c3c2fca101d9a3122317a
DMedak <dmedak15@gmail.com>
2026-05-21 13:10:07 +0200

    Popravljen Android build i konfiguriran produkcijski API

--- mobile/src/config/api.ts
+++ mobile/src/config/api.ts
@@ -1,11 +1,1 @@
-import Constants from 'expo-constants';
-
-const debuggerHost =
-  Constants.expoConfig?.hostUri ||
-  Constants.manifest2?.extra?.expoClient?.hostUri;
-
-const localIp = debuggerHost?.split(':')[0];
-
-export const API_URL = localIp
-  ? `http://${localIp}:3000`
-  : 'http://localhost:3000';
+export const API_URL = 'https://fitnesstracker-u1ec.onrender.com';
